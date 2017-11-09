081d2dcb7a21c291b1616153c3cc636c09f1b299
Olivia Beresford <oliviaberesford@hotmail.co.uk>
2017-11-09 11:29:08 +0000

    making the note controller

--- models/note-controller.js
+++ models/note-controller.js
@@ -1,4 +1,20 @@
 window.onload = function() {
-    var element = document.getElementById("app").textContent = "Howdy";
-  console.log(element);
+  // var element = document.getElementById("app").innerHTML = 'Howdy';
+  // console.log(element);
+  (function(exports) {
+    function NoteController() {
+      this.view = new NotelistView();
+    }
+
+    NoteController.prototype.addNote = function(title, text) {
+      this.view.noteList.saveNoteToList(title, text);
+    };
+
+    NoteController.prototype.createsView = function() {
+      return this.view.showAllNotes();
+    };
+    exports.NoteController = NoteController;
+  })(this);
+
+
 };
