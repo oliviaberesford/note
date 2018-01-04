bd423c3e420cce83d160fa8d100e4ebeec15dd41
Olivia Beresford <oliviaberesford@hotmail.co.uk>
2018-01-04 16:46:07 +0100

    need to fix problem

--- models/note-controller.js
+++ models/note-controller.js
@@ -6,15 +6,33 @@ window.onload = function() {
       this.view = new NotelistView();
     }
 
-    NoteController.prototype.addNote = function(title, text) {
+    var listed = document.getElementById('list-of-notes');
+    var title = document.getElementById('note-title').value;
+    var text = document.getElementById('note-text').value;
+
+    NoteController.prototype.addNote = function() {
       this.view.noteList.saveNoteToList(title, text);
     };
 
-    NoteController.prototype.createsView = function() {
-      return this.view.showAllNotes();
+    NoteController.prototype.listener = function() {
+      self = this;
+      function createsView(){
+        listed.textContent = self.view.showAllNotes();
+        console.log(self.view);
+      }
+      var button = document.getElementById('saveNote');
+      button.addEventListener('click', function(event){
+        event.preventDefault();
+        createsView();
+      }, false);
     };
-    exports.NoteController = NoteController;
-  })(this);
 
+      exports.NoteController = NoteController;
+
+      control = new NoteController();
+      control.addNote(title, text);
+      control.listener();
+
+  })(this);
 
 };
